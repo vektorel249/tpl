1a611d3d5102c1f074ec82943a58f0f3249f0316
vektorel249@outlook.com <vektorel249@outlook.com>
2025-04-13 09:49:05 +0300

    tpl task cancellation

--- Vektorel.Tpl/Vektorel.Tpl.FormApp/Form1.Designer.cs
+++ Vektorel.Tpl/Vektorel.Tpl.FormApp/Form1.Designer.cs
@@ -28,12 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnCancel = new Button();
+            btnWait = new Button();
+            btnFill = new Button();
+            txtNum1 = new TextBox();
+            txtNum2 = new TextBox();
+            txtSum = new TextBox();
+            lstNumbers = new ListBox();
+            btnSum = new Button();
+            SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(12, 388);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(75, 23);
+            btnCancel.TabIndex = 0;
+            btnCancel.Text = "Boşver";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // btnWait
+            // 
+            btnWait.Location = new Point(93, 388);
+            btnWait.Name = "btnWait";
+            btnWait.Size = new Size(75, 23);
+            btnWait.TabIndex = 1;
+            btnWait.Text = "Beklet";
+            btnWait.UseVisualStyleBackColor = true;
+            btnWait.Click += btnWait_Click;
+            // 
+            // btnFill
+            // 
+            btnFill.Location = new Point(174, 388);
+            btnFill.Name = "btnFill";
+            btnFill.Size = new Size(75, 23);
+            btnFill.TabIndex = 2;
+            btnFill.Text = "Doldur";
+            btnFill.UseVisualStyleBackColor = true;
+            // 
+            // txtNum1
+            // 
+            txtNum1.Location = new Point(255, 12);
+            txtNum1.Name = "txtNum1";
+            txtNum1.Size = new Size(153, 23);
+            txtNum1.TabIndex = 3;
+            // 
+            // txtNum2
+            // 
+            txtNum2.Location = new Point(255, 51);
+            txtNum2.Name = "txtNum2";
+            txtNum2.Size = new Size(153, 23);
+            txtNum2.TabIndex = 4;
+            // 
+            // txtSum
+            // 
+            txtSum.Location = new Point(255, 89);
+            txtSum.Name = "txtSum";
+            txtSum.ReadOnly = true;
+            txtSum.Size = new Size(153, 23);
+            txtSum.TabIndex = 5;
+            // 
+            // lstNumbers
+            // 
+            lstNumbers.FormattingEnabled = true;
+            lstNumbers.ItemHeight = 15;
+            lstNumbers.Location = new Point(12, 11);
+            lstNumbers.Name = "lstNumbers";
+            lstNumbers.Size = new Size(237, 364);
+            lstNumbers.TabIndex = 6;
+            // 
+            // btnSum
+            // 
+            btnSum.Location = new Point(333, 118);
+            btnSum.Name = "btnSum";
+            btnSum.Size = new Size(75, 23);
+            btnSum.TabIndex = 7;
+            btnSum.Text = "Topla";
+            btnSum.UseVisualStyleBackColor = true;
+            btnSum.Click += btnSum_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(423, 425);
+            Controls.Add(btnSum);
+            Controls.Add(lstNumbers);
+            Controls.Add(txtSum);
+            Controls.Add(txtNum2);
+            Controls.Add(txtNum1);
+            Controls.Add(btnFill);
+            Controls.Add(btnWait);
+            Controls.Add(btnCancel);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Text = "TPL";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnCancel;
+        private Button btnWait;
+        private Button btnFill;
+        private TextBox txtNum1;
+        private TextBox txtNum2;
+        private TextBox txtSum;
+        private ListBox lstNumbers;
+        private Button btnSum;
     }
 }
--- Vektorel.Tpl/Vektorel.Tpl.FormApp/Form1.cs
+++ Vektorel.Tpl/Vektorel.Tpl.FormApp/Form1.cs
@@ -6,5 +6,43 @@ namespace Vektorel.Tpl.FormApp
         {
             InitializeComponent();
         }
+
+        private CancellationTokenSource cts;
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            var sum = int.Parse(txtNum1.Text) + int.Parse(txtNum2.Text);
+            txtSum.Text = sum.ToString();
+        }
+
+        private async void btnWait_Click(object sender, EventArgs e)
+        {
+            cts = new CancellationTokenSource();
+            btnWait.Enabled = false;
+            try
+            {
+                //yalan iþ
+                await Task.Delay(5000, cts.Token);
+            }
+            catch (OperationCanceledException)
+            {
+                //sadece cancel iþleminden kaynaklý exceptionlarý görmezden gel (yut)
+            }
+            catch(NullReferenceException) 
+            {
+            }
+            catch(IndexOutOfRangeException)
+            {
+            }
+            btnWait.Enabled = true;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (cts is null)
+            {
+                return;
+            }
+            cts.Cancel();
+        }
     }
 }
